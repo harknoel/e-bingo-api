4c046253acf4a7dba4f8b4923f2621b11c53904a
harknoel <bontilao.harknoel@gmail.com>
2024-04-24 00:15:46 +0800

    Add game code

--- src/main/java/com/backend/bingo/model/BingoCard.java
+++ src/main/java/com/backend/bingo/model/BingoCard.java
@@ -1,21 +1,28 @@
 package com.backend.bingo.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
+import java.security.SecureRandom;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @Data
 @Entity
 public class BingoCard {
+
+    public BingoCard() {
+        this.bingoNumbers = initializeNumbers();
+        this.gameCode = generateGameCode();
+    }
+
     @Id
     @GeneratedValue
     private Integer bingoCardId;
+
+    private String gameCode;
 
     @ElementCollection
     private List<Integer> drawnNumbers;
@@ -23,8 +30,19 @@ public class BingoCard {
     @ElementCollection
     private List<Integer> bingoNumbers;
 
-    public BingoCard() {
-        this.bingoNumbers = initializeNumbers();
+    private static final String ALPHANUMERIC_CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public static String generateGameCode() {
+        Random random = new SecureRandom();
+        StringBuilder sb = new StringBuilder(8); // Length of the game code
+
+        for (int i = 0; i < 8; i++) {
+            int randomIndex = random.nextInt(ALPHANUMERIC_CHARACTERS.length());
+            char randomChar = ALPHANUMERIC_CHARACTERS.charAt(randomIndex);
+            sb.append(randomChar);
+        }
+
+        return sb.toString();
     }
 
     private List<Integer> initializeNumbers() {
