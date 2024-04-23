3098788cdc88d95078a6d80875ffacef315fe852
harknoel <bontilao.harknoel@gmail.com>
2024-04-24 00:15:28 +0800

    Add function in finding by game code

--- src/main/java/com/backend/bingo/repository/BingoCardRepository.java
+++ src/main/java/com/backend/bingo/repository/BingoCardRepository.java
@@ -3,5 +3,8 @@ package com.backend.bingo.repository;
 import com.backend.bingo.model.BingoCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BingoCardRepository extends JpaRepository<BingoCard, Integer> {
+    Optional<BingoCard> findByGameCode(String gameCode);
 }
