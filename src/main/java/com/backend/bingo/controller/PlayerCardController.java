2559a6971614664d6eaaf3aefe49884be0915596
harknoel <bontilao.harknoel@gmail.com>
2024-04-24 13:47:45 +0800

    Implement the function in creating the card

--- src/main/java/com/backend/bingo/controller/PlayerCardController.java
+++ src/main/java/com/backend/bingo/controller/PlayerCardController.java
@@ -1,9 +1,24 @@
 package com.backend.bingo.controller;
 
+import com.backend.bingo.payload.PlayerCardResponseDTO;
+import com.backend.bingo.service.PlayerCardService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/player")
 public class PlayerCardController {
+
+    private final PlayerCardService playerCardService;
+
+    public PlayerCardController(PlayerCardService playerCardService) {
+        this.playerCardService = playerCardService;
+    }
+
+    @GetMapping("/generate/{gameCode}")
+    public PlayerCardResponseDTO generateCard(@PathVariable String gameCode) {
+        return playerCardService.generateCard(gameCode);
+    }
 }
