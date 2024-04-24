4b2626894b6902fcfa57a150f8c6d86c01f04383
harknoel <bontilao.harknoel@gmail.com>
2024-04-24 13:46:11 +0800

    Implement generating card

--- src/main/java/com/backend/bingo/service/PlayerCardService.java
+++ src/main/java/com/backend/bingo/service/PlayerCardService.java
@@ -1,4 +1,20 @@
 package com.backend.bingo.service;
 
+import com.backend.bingo.model.PlayerCard;
+import com.backend.bingo.payload.PlayerCardResponseDTO;
+import com.backend.bingo.repository.PlayerCardRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class PlayerCardService {
+
+    private final PlayerCardRepository playerCardRepository;
+
+    public PlayerCardResponseDTO generateCard(String gameCode) {
+        PlayerCard playerCard = new PlayerCard(gameCode);
+        playerCardRepository.save(playerCard);
+        return new PlayerCardResponseDTO(playerCard.getPlayerToken(), playerCard.getPlayerNumbers());
+    }
 }
