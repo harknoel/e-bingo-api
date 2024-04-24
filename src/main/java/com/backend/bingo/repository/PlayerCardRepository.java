e88664ffbc5e4d9e44e8ae0d9cb10bdab312ef9c
harknoel <bontilao.harknoel@gmail.com>
2024-04-24 13:46:37 +0800

    Implement repository for player card

--- src/main/java/com/backend/bingo/repository/PlayerCardRepository.java
+++ src/main/java/com/backend/bingo/repository/PlayerCardRepository.java
@@ -1,4 +1,7 @@
 package com.backend.bingo.repository;
 
-public interface PlayerCardRepository {
+import com.backend.bingo.model.PlayerCard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlayerCardRepository extends JpaRepository<PlayerCard, Integer> {
 }
